bcba4ca2ce299747ac4d4bba7058e131e89adc2b
kalravm41 <64910029+kalravm41@users.noreply.github.com>
2020-05-24 15:29:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var ground,paper,log1,log2,log3;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1200, 800);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	paper = new Paper(100,600,40);
+	ground = new Ground(600,700,1300,20);
+	log1 = new Ground(850,620,20,150);
+	log2 = new Ground(1050,620,20,150);
+	log3 = new Ground(950,680,180,20);
+
+	//packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
+	//World.add(world, packageBody);
+	
+
+	//Create a Ground
+	//ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	//World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  paper.display();
+  ground.display();
+  log1.display();
+  log2.display();
+  log3.display();
+  keypressed();
+  drawSprites();
+ 
+}
+function keypressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:20,y:-20})
+	}
+}
+
+
